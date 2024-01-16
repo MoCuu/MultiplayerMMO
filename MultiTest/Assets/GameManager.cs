26aaf09c7bfa1a83cf25b82a0bdc3bbb5c9a1893
MoCuu <mocu245@gmail.com>
2024-01-16 18:17:16 +0100

    Project
    Dodałem zaczęty projekcik

---
+++ MultiTest/Assets/GameManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class GameManager : MonoBehaviour
+{
+    private void OnGUI() // Metoda która wywo³uje siê wraz z interfejsem
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if(!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) // Nie jestesmy klientem ani serwerem
+        //Singleton wzorzec oznaczaj¹cy ¿e zawsze jest tylko jedna instancja tego typu
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+            SubmitNewPosition();
+        }
+        GUILayout.EndArea();
+    }
+
+    static void StartButtons()
+    {
+        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+    }
+
+    static void StatusLabels() // Informacja w jakim trybie pracuje gra
+    {
+        var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client"; // Skrócony If (? = if true, : = else)
+
+        GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name); // Poka¿e w jaki sposób po³¹czony jest klient i serwer
+        GUILayout.Label("Mode: " + mode);
+    }
+
+    static void SubmitNewPosition()
+    {
+        string label;
+        if (NetworkManager.Singleton.IsServer)
+            label = "Move";
+        else
+            label = "New position request";
+
+        if(GUILayout.Button(label))
+        {
+            var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject(); // Zwraca objekt gracza
+            var player = playerObject.GetComponent<PlayerController>();
+            player.Move();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
